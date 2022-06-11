6fba842e6f2006d81a67e9ac7fed2b7c942a5ee7
Gabrieluza24 <gabrieluza24@gmail.com>
2022-06-11 12:26:11 -0400

    :sparkles: New Feature: Enciptacion de password antes de guardar

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,6 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from 'bcryptjs';
+
 
 @Entity('users')
 export class UserEntity {
@@ -12,13 +14,26 @@ export class UserEntity {
     readonly email: string;
 
     @Column()
-    readonly password: string;
+    password: string;
 
-    @CreateDateColumn({type: "timestamp"})
+    @CreateDateColumn({ type: "timestamp" })
     readonly createdAt: Date;
 
     constructor(email: string, password: string) {
         this.email = email;
         this.password = password;
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async hashPassword(): Promise<void> {
+        const salt = await bcrypt.genSalt();
+        if (!/^\$2a\$\d+\$/.test(this.password)) {
+            this.password = await bcrypt.hash(this.password, salt);
+        }
+    }
+
+    async checkPassword(plainPassword: string): Promise<boolean> {
+        return await bcrypt.compare(plainPassword, this.password);
     }
 }
